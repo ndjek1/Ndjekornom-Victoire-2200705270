9e3184bf870f6cfc24dd40cef089f933b2ecdf2c
ndjek <vndjekornom@gmail.com>
2024-06-05 12:38:03 +0300

    Day one

---
+++ Day 1/first.py
@@ -0,0 +1,14 @@
+print("Hello this is the first day of python")
+
+#variable declaration (A comment)
+name = "Coder"
+is_happy = True
+age = 20
+
+#declare function (A comment)
+def print_details(name, age):
+    print("Hello my name is" , name ," and i am " , age , " years old")
+
+#function call 
+
+print_details(name, age)
